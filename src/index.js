3a689ca99f81f807c1ddb6e33182cf02db166f4a
Maxim Terzhevik <maxt2000@mail.ru>
2022-07-21 00:18:53 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "../css/style.css";
 import { slider } from "./slider";
-import img1 from "./1.png";
-import img2 from "./2.png";
-import img3 from "./3.png";
-import img4 from "./4.png";
+import img1 from "../img/1.png";
+import img2 from "../img/2.png";
+import img3 from "../img/3.png";
+import img4 from "../img/4.png";
 
 const imgArr = [img1, img2, img3, img4];
 slider(imgArr);
